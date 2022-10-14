9c14971b16f9698a878938c2afb743d855c5126d
Carson Weeks <mail@carsonweeks.com>
2022-10-14 11:05:34 -0700

    Initial Commit.

---
+++ main.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { create } from "express-handlebars";
+
+const app = express();
+const hbs = create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.get("/", (req, res, next) => {
+  const planets = ["Earth", "Mercury", "Pluto", "Sun"];
+  res.render("planets", { planets: planets });
+});
+
+app.listen(3000, "localhost", () =>
+  console.log("running on http://localhost:3000")
+);
